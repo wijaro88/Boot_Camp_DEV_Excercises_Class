389c2557682e63ce46f836c060e97129c87543ce
Wilson R <wijaro88@gmail.com>
2023-06-20 21:06:04 -0500

    node_js

---
+++ 3.1.1_IntroANodeJS/script.js
@@ -0,0 +1,12 @@
+let x = 5;
+const PI = 3.1415;
+
+for (let i=0 ;i<x; i++){
+    console.log('hola mundo');
+}
+
+
+function saludo(nombre){
+    console.log(`hola ${nombre}`);  
+}
+saludo ('wilson');
