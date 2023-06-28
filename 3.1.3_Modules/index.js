e39f96dc9a157588964c621bd11183e9cd09172c
Wilson Rocha <wijaro88@gmail.com>
2023-06-27 22:14:46 -0500

    index

---
+++ 3.1.3_Modules/index.js
@@ -0,0 +1,8 @@
+const edad = 5;
+
+// opcion para que ese aarchivo solo exporte lo que hay luego de la asignacion
+//mala practica
+module.exports = edad;
+
+//Opcion para una variable
+// module.exports.edad = edad;
