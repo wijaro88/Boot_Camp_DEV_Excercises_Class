84d3304f0b169a9dea9ac42425beec76ae95028c
Wilson Rocha <wijaro88@gmail.com>
2023-07-17 11:43:50 -0500

    ECMA

--- 3.3.2_Api_Rest_Consumo/script.js
+++ 3.3.2_Api_Rest_Consumo/script.js
@@ -8,13 +8,15 @@ response.then((value)=>{
     // document.write(value.body);
     return value.json();
 }).then((valueJson)=> {
-    document.write(valueJson);
-    console.log(valueJson);
-    document.write(valueJson.ability(Name));
     document.write(valueJson.name);
+    
+    console.log(valueJson);
+    hr
+    document.write(valueJson.ability);
+    document.write(valueJson.base_experience);
     document.write(valueJson.height);
-    document.write(valueJson.order);
-    document.write(valueJson.types);
+    document.write(valueJson.id);
+    document.write(valueJson.weight);
 }).catch((err) => {
     document.write(err);
     document.write("Ocurrio un error");
