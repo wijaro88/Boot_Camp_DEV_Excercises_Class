f1aa096353b8c25d67937e9088fee57bfe1ee5a1
Wilson Rocha <wijaro88@gmail.com>
2023-09-21 20:52:37 -0500

    pokemon_style

--- 6.2.1_IntroUseEffect/src/App.jsx
+++ 6.2.1_IntroUseEffect/src/App.jsx
@@ -2,51 +2,20 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RouterIndex from './routes/RouterIndex'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
-  const [pokemones, setPokemons] = useState([]);
-  const [limite, setLimite] = useState(20);
-  const [filtroNombre] = useState('');
-
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/?&limit=${limite}`)
-    .then(response => response.json())
-    .then(jsonData => {
-      console.log(jsonData);
-      setPokemons(jsonData.results);
-    })
-    .catch(error => {
-      alert('No se pudo establecer conexión a la API. ');
-    })
-  }, [limite, pokemones]);
-
-  const capturarLimite = (event) => {
-    setLimite(event.target.value);
-  }
-
-  const capturarFiltroNombre = (event) => {
-    setFiltroNombre(event.target.value);
-  }
-
-  return (
-    <>
-      <h1>Pokedex</h1>
-      <label>Límite de Pokemones: </label>
-      <input 
-        value={limite}
-        onChange={capturarLimite}
-      />
-
-      <br/>
-      <label>Búsqueda por nombre: </label>
-      <input 
-        value={filtroNombre}
-        onChange={capturarFiltroNombre}
-      />
-    </>
-    
-  )
+ return(
+  <>
+  <BrowserRouter>
+    <RouterIndex/>
+  </BrowserRouter>
+  </>
+  
+  
+ )
 }
 
 export default App
