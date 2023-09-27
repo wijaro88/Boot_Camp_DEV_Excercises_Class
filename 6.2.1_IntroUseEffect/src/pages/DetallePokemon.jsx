c8b5139773acaa7230688528e3c1a14148838e78
Wilson Rocha <wijaro88@gmail.com>
2023-09-26 20:21:21 -0500

    add bootstrap

--- 6.2.1_IntroUseEffect/src/pages/DetallePokemon.jsx
+++ 6.2.1_IntroUseEffect/src/pages/DetallePokemon.jsx
@@ -2,37 +2,66 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../style.css';
 
 
 const DetallePokemon = () => {
 
-    const {id} = useParams();
+    const { id } = useParams();
 
     const [nombre, setNombre] = useState('');
     const [habilidad, setHabilidad] = useState('');
     const [altura, setAltura] = useState('');
+    const [stats, setStates] = useState([]);
+    const [detallesPokemon, setdetallesPokemon] = useState({});
+
+
 
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then(response => response.json())
-        .then(jsonData => {
-            console.log(jsonData);
-            setNombre(jsonData.name);
-            setAltura(jsonData.height);
-            setHabilidad(jsonData.abilities[0].ability.name);
-        })
-        .catch(error => console.log('Ocurrió un error en la consulta'));
+            .then(response => response.json())
+            .then(jsonData => {
+                console.log(jsonData);
+                setNombre(jsonData.name);
+                setAltura(jsonData.height);
+                setHabilidad(jsonData.abilities[0].ability.name);
+                setStates(jsonData.stats);
+                setdetallesPokemon(jsonData)
+            })
+            .catch(error => console.log('Ocurrió un error en la consulta'));
     })
 
     return (
         <>
-        <p><Link to={'/'}>Home </Link></p>
-        <h2>{nombre}</h2>
-        <p id='habilidad'>Habilidad: {habilidad}</p>
-        <p>Altura: {altura} cm</p>
-    </>
+            <p><Link to={'/'}>Home </Link></p>
+            <h2>{nombre}</h2>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${DetallePokemon.id}.png`}
+                    className='card-image-top'
+                    alt='Imagen de un pokemon'
+                  />
+            <table className="table">
+                <thead>
+
+                    <tr>
+                        <th>Estadistica</th>
+                        <th>Valor</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    {
+                        detallesPokemon.stats?.map((stat, index) => {
+                            return<tr key={index}>
+                                <td>{stat.stat.name}</td>
+                                <td>{stat.base_stat}</td>
+
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
--- 6.2.1_IntroUseEffect/src/pages/Home.jsx
+++ 6.2.1_IntroUseEffect/src/pages/Home.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    const [pokemones, setPokemones] = useState([]);
-    const [limite, setLimite] = useState(20);
-    const [filtroNombre] = useState('');
-    const [pokemonSeleccionado, setPokemonSeleccionado] = useState(0);
-  
-    useEffect(() => {
-      fetch(`https://pokeapi.co/api/v2/pokemon/?&limit=${limite}`)
+  const [pokemones, setPokemones] = useState([]);
+  const [limite, setLimite] = useState(20);
+  const [filtroNombre] = useState('');
+  const [pokemonSeleccionado, setPokemonSeleccionado] = useState(0);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?&limit=${limite}`)
       .then(response => response.json())
       .then(jsonData => {
         console.log(jsonData);
@@ -19,56 +19,65 @@ const Home = () => {
       })
     //}, [limite, pokemones]); // Dependencia circular. useEffect se va a llamar "a sí mismo" ya que
     //pokemones se modifica todo el tiempo.
-    }, [limite]);
-  
-    const capturarLimite = (event) => {
-      setLimite(event.target.value);
-    }
-  
-    const capturarFiltroNombre = (event) => {
-      setFiltroNombre(event.target.value);
-    }
+  }, [limite]);
 
-    const seleccionarPokemon = (idPokemon) => {
-        setPokemonSeleccionado(idPokemon);
-    }
-  
-    return (
-      <>
+  const capturarLimite = (event) => {
+    setLimite(event.target.value);
+  }
 
-        <p><Link to={'/about'}>Acerca de </Link></p>
-        <p><Link to={'/pokemon/2'}>Detalle Pokémon </Link></p>
+  const capturarFiltroNombre = (event) => {
+    setFiltroNombre(event.target.value);
+  }
 
-        <h1>Pokedex</h1>
-        <label>Límite de Pokemones: </label>
-        <input 
-          value={limite}
-          onChange={capturarLimite}
-        />
-  
-        {/*<label>Búsqueda por nombre: </label>
+  const seleccionarPokemon = (idPokemon) => {
+    setPokemonSeleccionado(idPokemon);
+  }
+
+  return (
+    <>
+
+      <p><Link to={'/about'}>Acerca de </Link></p>
+      <p><Link to={'/pokemon/2'}>Detalle Pokémon </Link></p>
+
+      <h1>Pokedex</h1>
+      <label>Límite de Pokemones: </label>
+      <input
+        value={limite}
+        onChange={capturarLimite}
+      />
+
+      {/*<label>Búsqueda por nombre: </label>
         <input 
           value={filtroNombre}
           onChange={capturarFiltroNombre}
         />*/}
-        
 
+      <div className='row'>
         {
-        pokemones.map((pokemon, index) => {
+          pokemones.map((pokemon, index) => {
             return (
-                <button 
-                    key={index} 
-                    onClick={() => seleccionarPokemon(pokemon.url)}
-                >
-                    <Link to={`/pokemon/${pokemon.url.split('/')[6]}`}>{pokemon.name}</Link>
-                </button>
+              <div className='col-6 col-sm-6 col-md-4'>
+                <div className='card ' key={index}>
+                  <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
+                    className='card-image-top'
+                    alt='Imagen de un pokemon'
+                  />
+                  <div className='card-body'>
+                    <Link
+                    className='card-title'
+                    to={`/pokemon/${pokemon.url.split('/')[6]}`}>{pokemon.name}</Link>
+                  </div>
+                </div>
+              </div>
             )
-        })
+
+          })
         }
-        { /*<DetallePokemon urlPokemon={pokemonSeleccionado}/> */}
-      </>
-      
-    )
+      </div>
+      { /*<DetallePokemon urlPokemon={pokemonSeleccionado}/> */}
+    </>
+
+  )
 }
 
 export default Home;
